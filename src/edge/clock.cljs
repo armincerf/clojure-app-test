8db1a9f0a97f5b5b1c65f2c3e2f8e91621dfbcc2
Alex Davis <armincerf@gmail.com>
2017-10-08 20:14:17 +0100

    add milliseconds, clean up code

--- src/edge/clock.cljs
+++ src/edge/clock.cljs
@@ -3,79 +3,76 @@
 
  (enable-console-print!)
 
-
-
 (def clock-state (r/atom {:current-time (js/Date.)}))
 
-(def stopwatch-state (atom 0)) ;0 represents pause, 1 start
+(def stopwatch-state (atom 0)) ;;0 represents pause, 1 start
+
+(def clock-atom (atom 0))
+
+(def start-time (atom 0))
+
+(def time-fn (atom inc)) ;; inc for stopwatch, dec for countdown
+
+(defonce interval (atom 0))
+
+
+
+(defn stopwatch-started? [] 
+  (if (= @stopwatch-state 0)
+    false
+    true))
 
 (defn tick [] dosync
   (swap! clock-state assoc :current-time (js/Date.)))
 
-(defn seconds-to-time
-  [secs]
-  (let [d (js/Date. (* secs 1000))]
+(defn seconds-to-time [secs]
+  (let [d (js/Date. (* 10 secs))]
     {:hours   (.getUTCHours d)
      :minutes (.getUTCMinutes d)
-     :seconds (.getUTCSeconds d)}))
+     :seconds (.getUTCSeconds d)
+     :milliseconds (.getUTCMilliseconds d)}))
 
-(defn display-time
-  "Pretty-prints minutes:seconds, given time map of the form {:hours x :seconds y}."
-  [tm]
+(defn display-time [tm]
   (let [pad (fn [n]
               (if (< n 10)
                 (str "0" n)
                 n))
         mm (pad (:minutes tm))
-        ss (pad (:seconds tm))]
-    (str mm ":" ss)))
-
-(def clock-atom (atom 0))
-(def start-time (atom 0))
-
-(def time-fn (atom inc)) ;; included so we can swap it out for countdowns
+        ss (pad (:seconds tm))
+        ms (pad (/ (:milliseconds tm) 10))]
+    (str mm ":" ss ":" ms)))
 
 (defn keep-time [func]
   (display-time (seconds-to-time @clock-atom))
   (swap! clock-atom func))
 
-(defonce interval (atom 0))
-
 (defn pause []
-  (if (= @stopwatch-state 1)
-    (do
-      (swap! stopwatch-state dec)
-      (js/clearInterval @interval))))
+  (when (stopwatch-started?)
+    (swap! stopwatch-state dec)
+    (js/clearInterval @interval)))
 
 (defn start []
-  (if (= @stopwatch-state 0)
-    (do
-      (swap! stopwatch-state inc)
-      (reset! interval (js/setInterval #(keep-time @time-fn) 1000)))))
+  (when-not (stopwatch-started?)
+    (swap! stopwatch-state inc)
+    (reset! interval (js/setInterval #(keep-time @time-fn) 10))))
 
 (defn reset []
   (reset! stopwatch-state 0)
   (js/clearInterval @interval)
   (reset! clock-atom @start-time))
 
-
-
-
-
-
  (defn clock []
-   (let [time-now (:current-time @clock-state)
-         val (r/atom 5)]
+   (let [time-now (:current-time @clock-state)]
      [:div.clock
-      [:h1 "time = " (str time-now)]
+      [:h1 "Date = " (str time-now)]
       [:h1  "stopwatch = " (display-time (seconds-to-time @clock-atom))]
-      [:h3 "Stopwatch state = " (str @stopwatch-state)]
       [:button {:on-click (fn [ev] (start))} "Start!"]
       [:button {:on-click (fn [ev] (pause))} "Pause!"]
       [:button {:on-click (fn [ev] (reset))} "Reset!"]]))
 
-(.setInterval js/window tick 500)
+
 
 (defn init [section]
- (r/render-component [clock] section))
+  (.setInterval js/window tick 100)
+  (r/render-component [clock] section))
 
